2d6f22c38864eb4681b883476b691c70c0f870b3
Станислав <kaopuk@gmail.com>
2021-01-15 11:49:46 +0300

    Start

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Stanislav Slipchenko on 15.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
